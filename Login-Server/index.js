70e9ebdb4e9c4eca52a73c69438781b783445fb9
drashti-dankhara <drashtidankhara7@gmail.com>
2023-04-08 15:38:22 +0530

    change

--- Login-Server/index.js
+++ Login-Server/index.js
@@ -1,10 +1,12 @@
 const connectToMongo = require("./db/conn.js");
 const express = require('express');
 const port = 5000;
-
 const app = express();
+
+//to work with json data
 app.use(express.json());
 
+//connection
 connectToMongo();
 
 app.listen(port, () => {
