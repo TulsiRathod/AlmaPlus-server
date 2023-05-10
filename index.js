6e6bb94a9c355e63255506909a120e53adb0e092
drashti-dankhara <drashtidankhara7@gmail.com>
2023-05-10 15:19:08 +0530

    socket added

--- index.js
+++ index.js
@@ -44,3 +44,47 @@ app.use(express.static('public'));
 app.listen(port, function () {
     console.log("Server is ready");
 })
+
+
+// socket-------------------------
+
+const io = require("socket.io")(8900, {
+    cors: {
+        origin: "http://localhost:3000"
+    },
+});
+let users = [];
+
+const addUser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) && users.push({ userId, socketId });
+}
+
+const removeUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId);
+}
+
+const getUser = (userId) => {
+    return users.find(user => user.userId === userId)
+}
+
+io.on("connection", (socket) => {
+    // console.log("a user connected!");
+    socket.on("addUser", userId => {
+        addUser(userId, socket.id);
+        io.emit("getUsers", users);
+    })
+
+    socket.on("disconnect", () => {
+        // console.log("user disconnected");
+        removeUser(socket.id);
+        io.emit("getUsers", users);
+    })
+
+    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+        const user = getUser(receiverId);
+        io.to(user.socketId).emit("getMessage", {
+            senderId,
+            text,
+        })
+    })
+})
